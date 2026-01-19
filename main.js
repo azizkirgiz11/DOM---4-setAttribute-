4db949f16fe935bb0d9cd60101da29abb51afcfe
azizkirgiz11 <azizkirgiz11@gmail.com>
2026-01-19 20:47:13 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function showImg() {
+    let img = document.getElementById("img");
+    img.setAttribute("src","https://shorthand.com/the-craft/raster-images/assets/5kVrMqC0wp/sh-unsplash_5qt09yibrok-4096x2731.jpeg");
+}
